47d150ff1b75107ee8a8507099ed8610e21f47e7
hesiyi <490052981@qq.com>
2016-12-26 14:31:28 +0800

    静态内部类

---
+++ MyThread/src/Chapter2/InnerStaticClass/Run.java
@@ -0,0 +1,19 @@
+package Chapter2.InnerStaticClass;
+/**
+ * Created by 49005 on 2016/12/23.
+ */
+public class Run {
+    public static void main(String[] args){
+        PublicClass publicClass=new PublicClass();
+        publicClass.setUsername("usernameValue");
+
+        publicClass.setPassword("passwordValue");
+        System.out.println(publicClass.getUsername()+" "+publicClass.getPassword());
+
+        //实例化内置类
+        PublicClass.PrivateClass privateClass=new PublicClass.PrivateClass();
+        privateClass.setAge("ageValue");
+        privateClass.setAddress("addressValue");
+        System.out.println(privateClass.getAge()+" "+privateClass.getAddress());
+    }
+}
---
+++ MyThread/src/Chapter2/InnerStaticClass/readme.txt
@@ -0,0 +1,4 @@
+静态内部类
+
+usernameValue passwordValue
+ageValue addressValue
