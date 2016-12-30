ee88c64fbad8455277965251b4acd3e77226272f
hesiyi <490052981@qq.com>
2016-12-30 15:02:32 +0800

    测试schedule方法任务不延时型 Date类型

---
+++ MyThread/src/Chapter5/TimerTest5/Test1.java
@@ -0,0 +1,31 @@
+package Chapter5.TimerTest5;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by 49005 on 2016/12/30.
+ */
+public class Test1 {
+    static class MyTask extends TimerTask{
+        @Override
+        public void run() {
+            try {
+                System.out.println("begin timer=" + System.currentTimeMillis());
+                Thread.sleep(1000);
+                System.out.println("end timer=" + System.currentTimeMillis());
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args){
+        MyTask task=new MyTask();
+        Calendar calendarRef=Calendar.getInstance();
+        Date date=calendarRef.getTime();
+        Timer timer=new Timer();
+        timer.schedule(task,date,3000);
+    }
+}
---
+++ MyThread/src/Chapter5/TimerTest5/readme.txt
@@ -0,0 +1,6 @@
+begin timer=1483081253024
+end timer=1483081254024
+begin timer=1483081256024
+end timer=1483081257025
+
+测试schedule方法任务不延时型 Date类型
