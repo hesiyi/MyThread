d697d324bb8fb74958013af9f04929fa1445ded8
hesiyi <490052981@qq.com>
2016-12-30 15:44:45 +0800

    Task任务不追赶

---
+++ MyThread/src/Chapter5/TimerTest5/Test9.java
@@ -0,0 +1,31 @@
+package Chapter5.TimerTest5;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by 49005 on 2016/12/30.
+ */
+public class Test9 {
+    static public class MyTask extends TimerTask{
+        @Override
+        public void run() {
+            System.out.println("begin timer="+System.currentTimeMillis());
+            System.out.println("  end timer="+System.currentTimeMillis());
+        }
+    }
+    public static void main(String[] args){
+        MyTask task=new MyTask();
+        System.out.println("现在执行时间为："+new Date());
+
+        Calendar calendarRef=Calendar.getInstance();
+        calendarRef.set(Calendar.SECOND,calendarRef.get(Calendar.SECOND)-20);
+
+        Date runDate=calendarRef.getTime();
+        System.out.println("计划时间为："+runDate);
+        Timer timer=new Timer();
+        timer.schedule(task,runDate,2000);
+    }
+}
